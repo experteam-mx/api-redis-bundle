55a9c73564d62e9e8c11e4f32be28d56eb2d1474
Enrique Hernandez <enrique.hernandez@experteam.com.ec>
2021-01-26 16:22:01 -0500

    refactor(redis_transport): added message base class.

---
+++ src/Message/Message.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Experteam\ApiRedisBundle\Message;
+
+abstract class Message
+{
+    /**
+     * @var mixed
+     */
+    private $data;
+
+    /**
+     * @param mixed $data
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+}
