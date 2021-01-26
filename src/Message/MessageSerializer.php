75d48aff1c7fc60a54b87a575f6b50fade32cb2b
Enrique Hernandez <enrique.hernandez@experteam.com.ec>
2021-01-26 18:23:01 -0500

    refactor(redis_transport): added messageHandler and messageSerializer base class.

---
+++ src/Message/MessageSerializer.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Experteam\ApiRedisBundle\Message;
+
+use Exception;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\Transport\Serialization\SerializerInterface;
+use Symfony\Component\Serializer\Encoder\DecoderInterface;
+use Symfony\Component\Serializer\Encoder\EncoderInterface;
+
+abstract class MessageSerializer implements SerializerInterface
+{
+    /**
+     * @var string
+     */
+    protected $messageClass;
+
+    /**
+     * @var DecoderInterface
+     */
+    protected $decoder;
+
+    /**
+     * @var EncoderInterface
+     */
+    protected $encoder;
+
+    /**
+     * @param DecoderInterface $decoder
+     * @param EncoderInterface $encoder
+     */
+    public function __construct(DecoderInterface $decoder, EncoderInterface $encoder)
+    {
+        $this->decoder = $decoder;
+        $this->encoder = $encoder;
+    }
+
+    /**
+     * @param array $encodedEnvelope
+     * @return Envelope
+     * @throws Exception
+     */
+    public function decode(array $encodedEnvelope): Envelope
+    {
+        $data = [];
+        $messageClass = $this->messageClass;
+
+        if (is_null($this->messageClass))
+            throw new Exception(sprintf('Empty message class. Please redefine and assign the messageClass field in %s', get_class($this)));
+
+        if (!class_exists($this->messageClass))
+            throw new Exception(sprintf('Message class "%s" does not exist', $messageClass));
+
+        if (isset($encodedEnvelope['body'])) {
+            $body = $this->decoder->decode($encodedEnvelope['body'], 'json');
+
+            if (isset($body['data'])) {
+                $data = $body['data'];
+            }
+        }
+
+        return new Envelope(new $messageClass($data));
+    }
+
+    /**
+     * @param Envelope $envelope
+     * @return array
+     * @throws Exception
+     */
+    public function encode(Envelope $envelope): array
+    {
+        $messageClass = $this->messageClass;
+
+        if (is_null($this->messageClass))
+            throw new Exception(sprintf('Empty message class. Please redefine and assign the messageClass field in %s', get_class($this)));
+
+        if (!class_exists($this->messageClass))
+            throw new Exception(sprintf('Message class "%s" does not exist', $messageClass));
+
+        $body = '';
+        $message = $envelope->getMessage();
+
+        if ($message instanceof $messageClass) {
+            $body = $this->encoder->encode(['data' => $message->getData()], 'json');
+        }
+
+        return ['body' => $body];
+    }
+}
