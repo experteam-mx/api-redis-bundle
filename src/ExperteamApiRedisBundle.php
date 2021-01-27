c0a23580e310beea0dd9b0b505e10395c88c6673
Enrique Hernandez <enrique.hernandez@experteam.com.ec>
2021-01-27 11:29:20 -0500

    refactor(bundle): removed doctrine compiler pass.

--- src/ExperteamApiRedisBundle.php
+++ src/ExperteamApiRedisBundle.php
@@ -4,15 +4,10 @@ namespace Experteam\ApiRedisBundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
 
 class ExperteamApiRedisBundle extends Bundle
 {
     public function build(ContainerBuilder $container)
     {
-        /*$container->addCompilerPass(DoctrineOrmMappingsPass::createAnnotationMappingDriver(
-            ['Experteam\ApiRedisBundle\Entity'],
-            [realpath(__DIR__.'/Entity')]
-        ));*/
     }
 }
