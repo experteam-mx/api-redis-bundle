0a13e97b56a27a394d0c7146a5883430b6eac747
Roberlay León Piñero <roberlayl@gmail.com>
2022-10-25 09:38:15 -0500

    feat: Improved refresh command

--- src/Command/RefreshCommand.php
+++ src/Command/RefreshCommand.php
@@ -3,16 +3,12 @@
 namespace Experteam\ApiRedisBundle\Command;
 
 use Exception;
-use Experteam\ApiRedisBundle\Service\RedisClient\RedisClientInterface;
-use Experteam\ApiRedisBundle\Service\RedisTransport\RedisTransportInterface;
 use Experteam\ApiRedisBundle\Service\RedisTransportV2\RedisTransportV2Interface;
-use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class RefreshCommand extends Command
 {
@@ -29,8 +25,7 @@ class RefreshCommand extends Command
 
     protected function configure()
     {
-        $this
-            ->setName('experteam:redis:refresh')
+        $this->setName('experteam:redis:refresh')
             ->setDescription('Refresh Redis data and queues')
             ->addOption('save', null, InputOption::VALUE_NONE, 'Refresh data structures')
             ->addOption('message', null, InputOption::VALUE_NONE, 'Refresh message queues')
@@ -38,8 +33,7 @@ class RefreshCommand extends Command
             ->addOption('from', null, InputOption::VALUE_REQUIRED, 'Created at from datetime. Format 2000-01-01T00:00:00')
             ->addOption('to', null, InputOption::VALUE_REQUIRED, 'Created at to datetime. Format 2000-01-01T00:00:00')
             ->addOption('entity', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'Entity to refresh')
-            ->addOption('id', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'Id to refresh')
-        ;
+            ->addOption('id', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'Id to refresh');
     }
 
     /**
@@ -56,30 +50,34 @@ class RefreshCommand extends Command
         $ids = $input->getOption('id');
 
         $namespace = "App\\Entity\\";
-        $entities = array_map(function(&$entity) use($namespace) {
-            return strpos($entity, $namespace) === false ? $namespace . $entity : $entity; 
+
+        $entities = array_map(function (&$entity) use ($namespace) {
+            return strpos($entity, $namespace) === false ? $namespace . $entity : $entity;
         }, $input->getOption('entity'));
 
         if ($input->getOption('save')) {
             $ui->text("<info>> Refreshing data structures...</info>");
-            
-            $this->redisTransport->restoreData($entities);
+            $keysNotGenerated = $this->redisTransport->restoreData($entities);
+
+            if (!is_null($keysNotGenerated)) {
+                $ui->error('The following redis keys were not generated => "' . implode(', ', $keysNotGenerated) . '".');
+                return Command::FAILURE;
+            }
         }
 
         if ($input->getOption('message')) {
             $ui->text("<info>> Refreshing message queues...</info>");
-            
+
             if (is_null($from)) {
                 $ui->error("<error>The --from option es required for --message</error>");
                 return Command::FAILURE;
             }
-            
+
             $this->redisTransport->restoreMessages($from, $to, $entities, $ids);
         }
 
         if ($input->getOption('stream_compute')) {
             $ui->text("<info>> Refreshing stream compute queues...</info>");
-            
             $this->redisTransport->restoreStreamCompute($from, $to, $entities, $ids);
         }
 
